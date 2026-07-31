48fa02da1e5bf82ae8581239bd673a04c066a4a0
ulyssescabette-code <ulyssescabette@ucnadv.com>
2026-07-31 12:48:52 -0300

    Delete uc-contracts directory

--- uc-contracts/src/lib/dataStore.js
+++
@@ -1,6 +0,0 @@
-import { isDemoMode } from './supabaseClient';
-import { db as demoDb } from './dataStore.demo';
-import { db as supabaseDb } from './dataStore.supabase';
-
-export { isDemoMode };
-export const db = isDemoMode ? demoDb : supabaseDb;
